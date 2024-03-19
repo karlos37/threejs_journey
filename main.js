1e55e10ff31c64e0d2a2f82a1c5f83e0b53f8dc4
Vedansh Surjan <vedansh008@gmail.com>
2024-03-19 09:45:15 -0500

    feat(Textures): Unfinished work of adding textures to materials

--- main.js
+++ main.js
@@ -2,11 +2,24 @@ import * as THREE from 'three'
 
 const canvas = document.querySelector('canvas.webgl');
 
-// Step 1 Add Scene
+// Add Scene
 const scene = new THREE.Scene();
+
+// Add Loading Manager
+const loadingManager = new THREE.LoadingManager();
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+const woodTexture = textureLoader.load('wood.avif');
+const paintTexture = textureLoader.load('paints.png');
+const paperTexture = textureLoader.load('paper.jpeg');
+
+woodTexture.colorSpace = THREE.SRGBColorSpace
+paintTexture.colorSpace = THREE.SRGBColorSpace
+paperTexture.colorSpace = THREE.SRGBColorSpace
+
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
+    map: woodTexture
 });
 
 // Create mesh by creating object and material
@@ -38,7 +51,7 @@ renderer.render(scene, camera);
 
 // Function that gets called once every frame
 const tick = function () {
-    mesh.rotation.y += 0.01;
+    // mesh.rotation.y += 0.01;
     // This gets called in the next frame
     window.requestAnimationFrame(tick);
 
